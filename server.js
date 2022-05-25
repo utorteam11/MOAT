9b21f2a05ccb929b2ec42c81a0e1b368943c3d1c
TaimurHasan <taimurhasan11@gmail.com>
2022-05-25 13:42:51 -0400

    adding sequelize connection and initial server connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+// const routes = require('./controllers'); <-- currently commented out as no routes
+const sequelize = require('./config/connection');
+
+// pending handlebars and sessions initialization
+
+// initialize express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set static folder for front end
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes); <-- currently commented out as no routes
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server is now listening on ${PORT}`));
+});
