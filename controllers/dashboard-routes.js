46e783cb64559cc90ddc89891c1a713d93f0a67f
TaimurHasan <taimurhasan11@gmail.com>
2022-05-27 20:10:35 -0400

    testing /dashboard/properties route for landlords

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('property-dashboard')
+router.get('/properties', (req, res) => {
+    // find properties where landlord_id is equal to req.session.user_id
+
+    const property = [{
+        address: "100 Somewhere Drive",
+        name: "Somewhere"
+    }]
+    res.render('properties', {
+        property,
+        loggedIn: true,
+        landlord: true,
+        first_name: 'Taimur'
+    })
 })
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage', {
-        loggedIn: false,
+        loggedIn: true,
         landlord: true
     })
 });
